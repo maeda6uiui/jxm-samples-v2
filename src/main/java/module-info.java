790aaf0ce48e9797f6bccaef23da825170d025dc
Dabasan <43123127+Dabasan@users.noreply.github.com>
2021-02-23 21:38:25 +0900

    Add module-info.java

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.github.dabasan.jxm_samples {
+	exports com.github.dabasan.jxm_samples;
+
+	requires com.github.dabasan.jxm.bd1;
+	requires com.github.dabasan.jxm.mif;
+	requires com.github.dabasan.jxm.pd1;
+	requires com.github.dabasan.jxm.properties;
+}
