ac18dc9f8d06bea11afa764dc9302b4de9f891b9
Dabasan <43123127+Dabasan@users.noreply.github.com>
2021-02-24 09:46:29 +0900

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,4 @@
 module com.github.dabasan.jxm_samples {
-	exports com.github.dabasan.jxm_samples;
-
 	requires com.github.dabasan.jxm.bd1;
 	requires com.github.dabasan.jxm.mif;
 	requires com.github.dabasan.jxm.pd1;
