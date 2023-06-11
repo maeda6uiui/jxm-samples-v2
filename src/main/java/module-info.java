53d98db66031e5f17eeb4fae93d2355c7e15ebf6
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2023-06-11 13:02:46 +0900

    Update

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.github.maeda6uiui.jxmsamplesv2 {
+    requires com.github.dabasan.jxm.bd1;
+    requires com.github.dabasan.jxm.mif;
+    requires com.github.dabasan.jxm.pd1;
+    requires com.github.dabasan.jxm.properties;
+
+    requires com.fasterxml.jackson.core;
+    requires com.fasterxml.jackson.databind;
+}
