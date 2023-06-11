72c85bd9801f7adfe634d673b1dd7cbfbb770d5d
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2023-06-11 13:02:31 +0900

    Update

--- src/main/java/module-info.java
+++
@@ -1,6 +0,0 @@
-module com.github.dabasan.jxm_samples {
-    requires com.github.dabasan.jxm.bd1;
-    requires com.github.dabasan.jxm.mif;
-    requires com.github.dabasan.jxm.pd1;
-    requires com.github.dabasan.jxm.properties;
-}
